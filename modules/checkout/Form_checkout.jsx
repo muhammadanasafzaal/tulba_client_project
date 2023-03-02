c794d0296492e08ea5de8ef2433af5bad8d7e4f5
usamasadiqus <usamasadiq029@gmail.com>
2023-03-02 21:41:20 +0500

    update checklist page according to figma design

--- modules/checkout/Form_checkout.jsx
+++ modules/checkout/Form_checkout.jsx
@@ -1,30 +1,31 @@
-import React from 'react'
-import { Container, Row, Col } from 'react-bootstrap';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import InputGroup from 'react-bootstrap/InputGroup';
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
 import styles from "styles/Form/Form.module.scss";
-
 
 const Form_checkout = () => {
   return (
     <>
-      <Container
-        fluid
-        className={`my-5 ${styles.forms}`}
-      >
-        <Row>
-          <Col lg={6} sm={12}className="checkForms p-5">
+      <Container className={`my-5 ${styles.forms}`}>
+        <Row className="flex justify-between items-center gap">
+          <Col lg={6} sm={12} className="checkForms p-0">
             <h1>CheckOut</h1>
-            <p className='mb-5'>All orders of your carts are here.</p>
+            <p className="mb-5">All orders of your carts are here.</p>
             <Form>
               <Row className="mb-3">
-                <Form.Group as={Col} lg={6} sm={12}  controlId="formGridEmail">
+                <Form.Group as={Col} lg={6} sm={12} controlId="formGridEmail">
                   <Form.Label>First Name</Form.Label>
                   <Form.Control type="text" value="Waleed" />
                 </Form.Group>
 
-                <Form.Group as={Col} lg={6} sm={12} controlId="formGridPassword">
+                <Form.Group
+                  as={Col}
+                  lg={6}
+                  sm={12}
+                  controlId="formGridPassword"
+                >
                   <Form.Label>Last Name</Form.Label>
                   <Form.Control type="text" />
                 </Form.Group>
@@ -35,12 +36,16 @@ const Form_checkout = () => {
                   <Form.Control type="email" />
                 </Form.Group>
 
-                <Form.Group as={Col} lg={6} sm={12} controlId="formGridPassword">
+                <Form.Group
+                  as={Col}
+                  lg={6}
+                  sm={12}
+                  controlId="formGridPassword"
+                >
                   <Form.Label>Address</Form.Label>
                   <Form.Control type="text" />
                 </Form.Group>
               </Row>
-
 
               <Row className="mb-3">
                 <Form.Group as={Col} lg={4} sm={12} controlId="formGridCity">
@@ -60,7 +65,6 @@ const Form_checkout = () => {
               </Row>
               <Row className="mb-3">
                 <Col lg={8}>
-
                   <Form.Group as={Col} controlId="formGridPassword">
                     <Form.Label>Phone Number</Form.Label>
                     <Form.Control type="number" />
@@ -68,16 +72,13 @@ const Form_checkout = () => {
                 </Col>
               </Row>
 
-              <Button type="submit"
-                className={`mt-4 ${styles.check_btn}`}
-              >
+              <Button type="submit" className={`mt-4 ${styles.check_btn}`}>
                 Continue
               </Button>
             </Form>
-
           </Col>
-          <Col lg={6} sm={12} xs={12}>
-            <div className={`my-5 p-5 ${styles.orderSummary}`}>
+          <Col lg={6} sm={12} xs={12} className="p-0">
+            <div className={`p-5 ${styles.orderSummary}`}>
               <h2>Order Summary</h2>
               <div className="d-flex justify-content-between py-3">
                 <span>Subtotal</span>
@@ -100,33 +101,38 @@ const Form_checkout = () => {
                 <h5>Total</h5>
                 <h5>$120</h5>
               </div>
-              <Button type="submit"
-                className={`mt-2 ${styles.check_btn1}`}
+              <button
+                type="submit"
+                className={`mt-2 text-white ${styles.check_btn1}`}
               >
                 Place Order
-              </Button>
-              <Row className="checkForms">
-                <Col lg={7} className="mt-4">
+              </button>
+              <Row className="checkForms my-2">
+                <Col lg={7} className="my-4">
                   <Form.Group as={Col} controlId="formGridPassword">
                     <Form.Label>Promo Code</Form.Label>
                     <Form.Control type="text" />
                   </Form.Group>
                 </Col>
                 <Col lg={5} className="mt-5">
-                  <Button variant="outline-danger" className={`mt-2 w-28 ${styles.applyBtn} `}>Apply Now</Button>{' '}
+                  <Button
+                    variant="outline-danger"
+                    className={`mt-2 w-28 ${styles.applyBtn} `}
+                  >
+                    Apply Now
+                  </Button>{" "}
                 </Col>
               </Row>
-              <div className='my-3'>
+              <div className="my-3">
                 <span className={styles.acc}>Don’t have an account ? </span>
-               <a href="#"> Create New</a>
-                </div>
+                <a href="#"> Create New</a>
+              </div>
             </div>
           </Col>
         </Row>
       </Container>
-
     </>
-  )
-}
+  );
+};
 
-export default Form_checkout
+export default Form_checkout;
--- modules/checkout/Payment.jsx
+++ modules/checkout/Payment.jsx
@@ -1,51 +1,80 @@
-import React from 'react'
-import { Container, Row, Col } from 'react-bootstrap'
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 import styles from "styles/Form/Form.module.scss";
-import Table from 'react-bootstrap/Table';
+import Table from "react-bootstrap/Table";
 
 const Payment = () => {
-    return (
-        <>
-            <Container fluid>
-                <Row>
-                    <Col>
-                        <div className={`p-4  ${styles.payment}`}>
-                            <h3 className='my-4 '>Payment</h3>
-                            <hr />
-                            <h3 className='my-4 '>Your Order</h3>
-                            <Table className=''>
-      <thead>
-        <tr >
-          <th>Items</th>
-          <th>Unit price</th>
-          <th>Quantity</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td> Nullam maximus sodales enim, sollicitudin congue erat malesuada sed</td>
-          <td>$120</td>
-          <td>1</td>
-        </tr>
-        <tr>
-          <td> Nullam maximus sodales enim, sollicitudin congue erat malesuada sed</td>
-          <td>$120</td>
-          <td>1</td>
-        </tr>
-        <tr>
-          <td> Nullam maximus sodales enim, sollicitudin congue erat malesuada sed</td>
-          <td>$120</td>
-          <td>1</td>
-        </tr>
-      </tbody>
-    </Table>
+  return (
+    <>
+      <Container>
+        <Row>
+          <Col className="p-0">
+            <div className={`p-4 ${styles.payment}`}>
+              <h3 className="my-4">Payment</h3>
+              <hr />
+              <h3 className="my-4">Your Order</h3>
+              <Table>
+                <thead>
+                  <tr>
+                    <th>Items</th>
+                    <th className="text-center">Unit price</th>
+                    <th className="text-center">Quantity</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>
+                      {" "}
+                      Nullam maximus sodales enim, sollicitudin congue erat
+                      malesuada sed
+                    </td>
+                    <td className="text-center">$120</td>
+                    <td className="text-center">1</td>
+                  </tr>
+                  <tr>
+                    <td>
+                      {" "}
+                      Nullam maximus sodales enim, sollicitudin congue erat
+                      malesuada sed
+                    </td>
+                    <td className="text-center">$120</td>
+                    <td className="text-center">1</td>
+                  </tr>
+                  <tr>
+                    <td>
+                      {" "}
+                      Nullam maximus sodales enim, sollicitudin congue erat
+                      malesuada sed
+                    </td>
+                    <td className="text-center">$120</td>
+                    <td className="text-center">1</td>
+                  </tr>
+                  <tr>
+                    <td>
+                      {" "}
+                      Nullam maximus sodales enim, sollicitudin congue erat
+                      malesuada sed
+                    </td>
+                    <td className="text-center">$120</td>
+                    <td className="text-center">1</td>
+                  </tr>
+                  <tr>
+                    <td>
+                      {" "}
+                      Nullam maximus sodales enim, sollicitudin congue erat
+                      malesuada sed
+                    </td>
+                    <td className="text-center">$120</td>
+                    <td className="text-center">1</td>
+                  </tr>
+                </tbody>
+              </Table>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+};
 
-                        </div>
-                    </Col>
-                </Row>
-            </Container>
-        </>
-    )
-}
-
-export default Payment
+export default Payment;

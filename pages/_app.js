4faba4948850b119f93ebdd31067fddafe754797
AbdulMoizJawed <86627657+AbdulMoizJawed@users.noreply.github.com>
2023-03-24 22:52:42 +0500

    Update _app.js
    CrossOrigin converted to camel case

--- pages/_app.js
+++ pages/_app.js
@@ -13,7 +13,7 @@ function MyApp({ Component, pageProps }) {
         <meta name="description" content="And we created you in pairs" />
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
           href="https://fonts.googleapis.com/css2?family=DM+Serif+Display:ital@1&display=swap"
           rel="stylesheet"

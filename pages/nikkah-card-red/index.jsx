373216583f6ea3104ae16b6b5435c71495942f9a
abdulmoizjawed <amoizj16@gmail.com>
2023-03-25 06:24:10 +0500

    Nikkah card red route updated

--- pages/nikkah-card-red/index.jsx
+++ pages/nikkah-card-red/index.jsx
@@ -1,9 +1,9 @@
 import Footer from "layout/footer";
 import Header from "layout/header";
-import AboutEvent from "modules/nikkah-card/AboutEvent";
-import CardSection from "modules/nikkah-card/CardSection";
-import InfoForm from "modules/nikkah-card/InfoForm";
-import SpecialEvent from "modules/nikkah-card/SpecialEvent";
+import AboutEvent from "modules/nikkah-card-red/AboutEvent";
+import CardSection from "modules/nikkah-card-red/CardSection";
+import InfoForm from "modules/nikkah-card-red/InfoForm";
+import SpecialEvent from "modules/nikkah-card-red/SpecialEvent";
 import React from "react";
 
 const NikkahTemplate = () => {
@@ -14,7 +14,6 @@ const NikkahTemplate = () => {
       <SpecialEvent />
       <AboutEvent />
       <InfoForm />
-
     </div>
   );
 };
